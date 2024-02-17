5216d7f6b2cc90d554bd15955b6d9e3ef994e34d
Vince Chu <chu.vincent1@gmail.com>
2024-02-16 22:23:44 -0500

    Add logo animation

---
+++ js/logo_animation.js
@@ -0,0 +1,19 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+console.log(document.querySelector("#logo").innerText);
+document.querySelector("#logo").onmouseover = event => {
+    let iterations = 0;
+    const interval = setInterval(() => {
+        event.target.innerText = event.target.innerText.split("")
+            .map((letter, index) => {
+                if (index < iterations) {
+                    return event.target.dataset.value[index];
+                }
+                return letters[Math.floor(Math.random() * 26)];
+            })
+            .join("");
+        if (iterations >= event.target.dataset.value.length) {
+            clearInterval(interval);
+        }
+        iterations += 1/2;
+    }, 30);
+}
